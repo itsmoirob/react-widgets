737c52346cb71fd5116f45f71715c1589964fa15
Robbie <rob.w.sherman@gmail.com>
2020-11-22 21:44:09 +0000

    Add basic accordion

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordian = ({ items }) => {
-  const renderedItems = items.map(i =>
-    (<React.Fragment key={i.title}>
-      <div className="title active">
-        <i className="dropdown icon"></i>
-        {i.title}
-      </div>
+  const [activeIndex, setActiveIndex] = useState(null);
 
-      <div className="content active">
-        <p>{i.content}</p>
-      </div>
-    </React.Fragment>)
-  );
+  const onTitleClick = (idx) => {
+    setActiveIndex(idx);
+  };
+
+  const renderedItems = items.map((i, idx) => {
+    const isActive = activeIndex === idx ? 'active' : '';
+
+    return (
+      <React.Fragment key={i.title}>
+        <div className={`${isActive} title`} onClick={e => onTitleClick(idx)}>
+          <i className="dropdown icon"></i>
+          {i.title} : {isActive}
+        </div>
+
+        <div className={`${isActive} content`}>
+          <p>{i.content}</p>
+        </div>
+      </React.Fragment>
+    )
+  });
 
   return (
     <div className="ui styled accordion">
