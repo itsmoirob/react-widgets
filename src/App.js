dd2ca9efaac4cdb60da230783f5c30e411c148c2
Robbie <rob.w.sherman@gmail.com>
2020-11-23 05:49:28 +0000

    Add Search component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import Accordian from './components/Accordian';
+// import Accordian from './components/Accordian';
+import Search from './components/Search';
 
-const items = [
-  { title: 'What is React?', content: 'React is a front end javascript framework' },
-  { title: 'Why use React?', content: 'React is a favourite javascript lirary among engineers' },
-  { title: 'How do you use React?', content: 'You use React by creating componenets' },
-];
+// const items = [
+//   { title: 'What is React?', content: 'React is a front end javascript framework' },
+//   { title: 'Why use React?', content: 'React is a favourite javascript lirary among engineers' },
+//   { title: 'How do you use React?', content: 'You use React by creating componenets' },
+// ];
 
 export default () => {
   return (
     <div>
-      <Accordian items={items} />
+      <Search />
     </div>
   )
 }
