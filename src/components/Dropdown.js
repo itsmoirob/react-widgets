e0a99364ffcf1e7c705fd5a1983787b2b5a90764
Robbie <rob.w.sherman@gmail.com>
2020-11-24 06:58:33 +0000

    Cloe dropdown when clicked outside

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      if (ref.current?.contains(event.target)) return;
+      setOpen(false)
+    }, { capture: true })
+  }, [])
+
   const renderedOptions = options.map(o => {
-    if (o.value === selected.value) return;
+    if (o.value === selected.value) return null;
 
     return (
       <div key={o.value} className="item" onClick={() => onSelectedChange(o)}>
@@ -11,10 +21,8 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     )
   });
 
-  const [open, setOpen] = useState(false);
-
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a colour</label>
         <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
