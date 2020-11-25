4128cd7c709be08e96183cc6b0ae25e48f7713fe
Robbie <rob.w.sherman@gmail.com>
2020-11-25 06:41:54 +0000

    Add some routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
 
-// import Accordian from './components/Accordian';
-// import Search from './components/Search';
+import Accordian from './components/Accordian';
+import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
-// const items = [
-//   { title: 'What is React?', content: 'React is a front end javascript framework' },
-//   { title: 'Why use React?', content: 'React is a favourite javascript lirary among engineers' },
-//   { title: 'How do you use React?', content: 'You use React by creating componenets' },
-// ];
+const items = [
+  { title: 'What is React?', content: 'React is a front end javascript framework' },
+  { title: 'Why use React?', content: 'React is a favourite javascript lirary among engineers' },
+  { title: 'How do you use React?', content: 'You use React by creating componenets' },
+];
 
-// const options = [
-//   { label: 'The colour red', value: 'red' },
-//   { label: 'The colour green', value: 'green' },
-//   { label: 'A shade of blue', value: 'blue' },
-// ]
+const options = [
+  { label: 'The colour red', value: 'red' },
+  { label: 'The colour green', value: 'green' },
+  { label: 'A shade of blue', value: 'blue' },
+]
+
 
 const App = () => {
-  // const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Translate />
-      {/* <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> */}
+      <Route path='/'><Accordian items={items} /></Route>
+      <Route path='/translate'><Translate /></Route>
+      <Route path='/dropdown'>
+        <Dropdown
+          options={options}
+          title="Pick a colour"
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path='/list'><Search /></Route>
     </div>
   )
 }
