6d2a8dc2bd35a3c5f5548a9a4b56285cb8093263
Robbie <rob.w.sherman@gmail.com>
2020-11-25 06:21:43 +0000

    Fetch translation and update page

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'redaxios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
   useEffect(() => {
-    console.log('change')
-   }, [language, text]);
+    const timerId = setTimeout(() => {
+      setDebouncedText(text)
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: debouncedText,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      });
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+
+  }, [language, debouncedText]);
 
   return (
-    <div>Convert</div>
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
   )
 }
 
